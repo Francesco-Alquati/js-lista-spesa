5733e34747b90d493c07cf8c423ecad8328cbddc
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-02 15:58:18 +0200

    [UPD] Get ul element from DOM

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 // ARRAY CON LA LISTA DELLA SPESA
 const spesa = ['pasta', 'mozzarella', 'limone', 'pane', 'banane', 'yogurt', 'latte', 'uova'];
 
+// RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA LISTA DELLA SPESA
+let lista_spesa = document.getElementById('lista-spesa');
