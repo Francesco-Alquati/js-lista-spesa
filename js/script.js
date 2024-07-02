0463602a65dcf4354808ff436d44a0cb11fd5b46
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-02 16:10:07 +0200

    [UPD] While loop with increment to the last element of the list

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ const spesa = ['pasta', 'mozzarella', 'limone', 'pane', 'banane', 'yogurt', 'lat
 
 // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA LISTA DELLA SPESA
 let lista_spesa = document.getElementById('lista-spesa');
+
+// CREO LA VARIABILE INDICE
+let i = 0;
+
+// CICLO L'ARRAY
+while(i<spesa.length){
+    // CREO  UN NUOVO LIST ITEM
+    const li = document.createElement('li');
+
+    // GLI METTO IL TESTO ALL'INTERNO
+    li.innerText = spesa[i];
+
+    // INCREMENTO LA VARIABILE INDICE
+    i++;
+
+    
+}
